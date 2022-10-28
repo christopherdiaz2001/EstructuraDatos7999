b63e605334598d34667eb52e420de9d9574fa563
Widinson Danilo <widinsondanilo@gmail.com>
2022-10-27 22:51:42 -0500

    Modelado y cpp y h de PersonaEdad

---
+++ PersonaEdad.h
@@ -0,0 +1,37 @@
+/***********************************************************************
+ * Module:  PersonaEdad.h
+ * Author:  widin
+ * Modified: jueves, 27 de octubre de 2022 09:26:09 p. m.
+ * Purpose: Declaration of the class PersonaEdad
+ ***********************************************************************/
+
+#if !defined(__Class_Diagram_1_PersonaEdad_h)
+#define __Class_Diagram_1_PersonaEdad_h
+
+class PersonaEdad
+{
+public:
+   char getNombre(void);
+   void setNombre(char newNombre);
+   int getAnoNacimiento(void);
+   void setAnoNacimiento(int newAnoNacimiento);
+   int getAnoActual(void);
+   void setAnoActual(int newAnoActual);
+   PersonaEdad(int anoNaci, int anoAact, char nombr, int edad);
+   ~PersonaEdad();
+   PersonaEdad calculaEdad(PersonaEdad nombr, PersonaEdad anoNaci,PersonaEdad anoActu, PersonaEdad edad);
+   void imprimir(void);
+   int getEdad(void);
+   void setEdad(int newEdad);
+
+protected:
+private:
+   char nombre;
+   int anoNacimiento;
+   int anoActual;
+   int edad;
+
+
+};
+
+#endif
