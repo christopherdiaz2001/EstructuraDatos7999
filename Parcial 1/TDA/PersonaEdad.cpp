d7da20c2bade3c3961ad8718fbbf5b64d6ad065e
Christopher <67701584+christopherdiaz2001@users.noreply.github.com>
2022-10-28 02:49:14 -0500

    Add files via upload

---
+++ Parcial 1/TDA/PersonaEdad.cpp
@@ -0,0 +1,87 @@
+/***********************************************************************
+ * Module:  PersonaEdad.cpp
+ * Author:  widin
+ * Modified: jueves, 27 de octubre de 2022 09:26:09 p. m.
+ * Purpose: Implementation of the class PersonaEdad
+ ***********************************************************************/
+
+#include "PersonaEdad.h"
+#include <iostream>
+using namespace std;
+
+
+char PersonaEdad::getNombre(void)
+{
+   return nombre;
+}
+
+
+void PersonaEdad::setNombre(char newNombre)
+{
+   nombre = newNombre;
+}
+
+
+
+int PersonaEdad::getAnoNacimiento(void)
+{
+   return anoNacimiento;
+}
+
+
+
+void PersonaEdad::setAnoNacimiento(int newAnoNacimiento)
+{
+   anoNacimiento = newAnoNacimiento;
+}
+
+
+int PersonaEdad::getAnoActual(void)
+{
+   return anoActual;
+}
+
+
+void PersonaEdad::setAnoActual(int newAnoActual)
+{
+   anoActual = newAnoActual;
+}
+
+
+PersonaEdad::PersonaEdad(int anoNaci, int anoAact, char nombr, int edad)
+{
+ this->anoNacimiento = anoNaci;
+   this->anoActual = anoAact;
+   this->nombre = nombr;
+   this->edad = edad;
+}
+
+
+PersonaEdad::~PersonaEdad()
+{
+   // TODO : implement
+}
+
+PersonaEdad PersonaEdad::calculaEdad(PersonaEdad obj1, PersonaEdad obj2)
+{
+ this->setEdad(obj2.getAnoActual() - obj1.getAnoNacimiento());
+   return *this;
+}
+
+
+void PersonaEdad::imprimir(void)
+{
+   cout<<"Edad: "<<this->edad<<endl;
+}
+
+
+int PersonaEdad::getEdad(void)
+{
+   return edad;
+}
+
+
+void PersonaEdad::setEdad(int newEdad)
+{
+   edad = newEdad;
+}
---
+++ Parcial 1/TDA/main.cpp
@@ -0,0 +1,11 @@
+#include "PersonaEdad.h"
+#include "PersonaEdad.cpp"
+#include <iostream>
+using namespace std;
+main(int argc, char** argv)
+{
+    PersonaEdad *obj1 = new PersonaEdad(2002, 2022, 'W', 0);
+	obj1 -> calculaEdad (obj1, obj2);
+    obj1 -> imprimir();
+    return 0;
+}
